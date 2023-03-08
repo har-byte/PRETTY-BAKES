531b0339eb038dda1a84e9955a1cf9e6b531ddfc
enlous <mssneha260@gmail.com>
2023-03-08 12:37:52 +0530

    Pretty Bake to push to git hub

---
+++ FRONTEND/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from FRONTEND import views
+
+urlpatterns = [
+    path('', views.homepage, name="homepage"),
+    path('about/', views.about, name="about"),
+    path('contact/', views.contact, name="contact"),
+    path('discategory/<itemCatg>/', views.discategory, name="discategory"),
+    path('details/<int:dataid>/', views.details, name="details"),
+    path('form/', views.form, name="form"),
+    path('formreg/', views.formreg, name="formreg"),
+    path('customerlogin/', views.customerlogin, name="customerlogin"),
+    path('customerlogout/', views.customerlogout, name="customerlogout"),
+    path('condet/', views.condet, name="condet")
+
+
+    ]
